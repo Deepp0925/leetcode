b5ebdc84384812eb54adf02114bfa5ee708e42c9
Deep Patel <deepp0925@gmail.com>
2024-02-03 21:12:44 -0500

    some more problems solved

--- src/num30.rs
+++ src/num30.rs
@@ -1,35 +1,127 @@
-pub fn s(s: String, words: Vec<String>) -> Vec<usize> {
-    fn permute(arr: &mut Vec<String>, start: usize) {
-        if start == arr.len() - 1 {
-            return;
+pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+    // shift each window by the word len in every iteration
+    let word_len = words[0].len();
+    // this is also the window len
+    let permutation_len = word_len * words.len();
+
+    let mut result = vec![];
+
+    if permutation_len > s.len() {
+        return result;
+    }
+
+    let mut mapped_words = std::collections::HashMap::with_capacity(words.len());
+
+    for word in words {
+        if let Some(val) = mapped_words.get_mut(&word) {
+            *val += 1;
+        } else {
+            mapped_words.insert(word, 1);
+        }
+    }
+
+    let has_permutation = |s: &str| -> bool {
+        let mut mapped_window_items = std::collections::HashMap::with_capacity(s.len() / word_len);
+
+        for i in (0..s.len()).step_by(word_len) {
+            let mini_window = &s[i..i + word_len];
+            if let Some(val) = mapped_window_items.get_mut(&mini_window) {
+                *val += 1;
+            } else {
+                mapped_window_items.insert(mini_window, 1);
+            }
         }
 
-        for i in start..arr.len() {
-            arr.swap(start, i);
-            permute(arr, start + 1);
-            arr.swap(start, i);
+        for (item, count) in mapped_window_items {
+            if let Some(mapped_word_val) = mapped_words.get(item) {
+                if &count != mapped_word_val {
+                    return false;
+                }
+            } else {
+                return false;
+            };
+        }
+
+        true
+    };
+
+    for i in 0..s.len() {
+        let range = i..i + permutation_len;
+        if range.end > s.len() {
+            break;
+        }
+        let s = &s[range];
+        if has_permutation(s) {
+            result.push(i as i32);
         }
     }
-    todo!()
-}
 
-fn permute(arr: &mut Vec<String>, start: usize) {
-    if start == arr.len() - 1 {
-        return;
-    }
-
-    for i in start..arr.len() {
-        arr.swap(start, i);
-        permute(arr, start + 1);
-        arr.swap(start, i);
-    }
+    result
 }
 
 #[test]
-fn test_permute() {
-    let mut strs = vec!["ab".to_string(), "cd".to_string(), "ef".to_string()];
+fn test_find_substring() {
+    assert_eq!(
+        find_substring(
+            "barfoothefoobarman".to_string(),
+            vec!["foo".to_string(), "bar".to_string()]
+        ),
+        vec![0, 9]
+    );
 
-    permute(&mut strs, 0);
+    assert_eq!(
+        find_substring(
+            "wordgoodgoodgoodbestword".to_string(),
+            vec![
+                "word".to_string(),
+                "good".to_string(),
+                "best".to_string(),
+                "word".to_string()
+            ]
+        ),
+        vec![]
+    );
 
-    println!("{strs:?}");
+    assert_eq!(
+        find_substring(
+            "barfoofoobarthefoobarman".to_string(),
+            vec!["foo".to_string(), "bar".to_string(), "the".to_string(),]
+        ),
+        vec![6, 9, 12]
+    );
+
+    assert_eq!(
+        find_substring(
+            "wordgoodgoodgoodbestword".to_string(),
+            vec![
+                "word".to_string(),
+                "good".to_string(),
+                "best".to_string(),
+                "good".to_string()
+            ]
+        ),
+        vec![8]
+    );
+
+    assert_eq!(
+        find_substring(
+            "lingmindraboofooowingdingbarrwingmonkeypoundcake".to_string(),
+            vec![
+                "fooo".to_string(),
+                "barr".to_string(),
+                "wing".to_string(),
+                "ding".to_string(),
+                "wing".to_string()
+            ]
+        ),
+        vec![13]
+    );
+
+    assert_eq!(
+        find_substring(
+            "ababaab".to_string(),
+            vec!["ab".to_string(), "ba".to_string(), "ba".to_string(),]
+        ),
+        vec![1]
+    );
 }
--- src/num5.rs
+++ src/num5.rs
@@ -4,31 +4,31 @@ pub fn longest_palindrome(s: String) -> String {
         return s;
     }
 
-    let mut longest = String::new();
-    for i in 0..len {
-        let mut left = i;
-        let mut right = i;
-
-        while left > 0 && s.chars().nth(left - 1) == s.chars().nth(i) {
-            left -= 1;
+    fn expand_around_center(s: &str, l: usize, mut r: usize) -> usize {
+        let mut l = l as i32;
+        while l >= 0 && r < s.len() && s[l as usize..l as usize + 1] == s[r..r + 1] {
+            l -= 1;
+            r += 1;
         }
 
-        while right < len - 1 && s.chars().nth(right + 1) == s.chars().nth(i) {
-            right += 1;
-        }
-
-        while left > 0 && right < len - 1 && s.chars().nth(left - 1) == s.chars().nth(right + 1) {
-            left -= 1;
-            right += 1;
-        }
-
-        let curr = &s[left..=right];
-        if curr.len() > longest.len() {
-            longest = curr.to_string();
-        }
+        (r as i32 - l - 1) as usize
     }
 
-    longest
+    let mut start = 0;
+    let mut max_len = 0;
+
+    for i in 0..len {
+        // check for odd lenght palindromes
+        let l1 = expand_around_center(&s, i, i);
+        let l2 = expand_around_center(&s, i, i + 1);
+
+        let max = l1.max(l2);
+        if max > max_len {
+            max_len = max;
+            start = i - ((max - 1) / 2);
+        }
+    }
+    s[start..start + max_len].to_string()
 }
 
 #[cfg(test)]
@@ -37,17 +37,13 @@ mod tests {
 
     #[test]
     fn test_longest_palindrome() {
-        assert_eq!(longest_palindrome("babad".to_string()), "aba".to_string());
+        assert_eq!(longest_palindrome("babad".to_string()), "bab".to_string());
         assert_eq!(longest_palindrome("cbbd".to_string()), "bb".to_string());
         assert_eq!(longest_palindrome("a".to_string()), "a".to_string());
         assert_eq!(longest_palindrome("ac".to_string()), "a".to_string());
-        let s = "z".repeat(100);
+        let s = "z".repeat(4);
         assert_eq!(longest_palindrome(s.clone()), s);
-    }
-
-    #[test]
-    fn max_test() {
-        println!("{}", std::cmp::max(1, 2));
-        println!("{}", std::cmp::max("A", "B"));
+        let s = "c".repeat(3);
+        assert_eq!(longest_palindrome(s.clone()), s);
     }
 }
