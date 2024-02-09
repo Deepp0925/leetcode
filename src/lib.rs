4c102438318c52806bae96a7e646f0ef08ac16d9
Deep Patel <deepp0925@gmail.com>
2024-02-08 22:29:52 -0500

    first missing positive

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,27 @@
 mod solutions_1_30;
-//  void combSum(int i,int target,vector<int>& arr,vector<int>& comb,vector<vector<int>>& ans,int n)
-// {
-//     if(i==n)
-//     {
-//         return;
-//     }
-//     if(target==0)
-//     {
-//         ans.push_back(comb);
-//         return;
-//     }
-//     if(arr[i]<=target)
-//     {
-//         comb.push_back(arr[i]);
-//         combSum(i,target-arr[i],arr,comb,ans,n);
-//         comb.pop_back();
-//     }
-//     combSum(i+1,target,arr,comb,ans,n);
-// }
-// vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-//     vector<vector<int>> ans;
-//     vector<int> comb;
-//     int n=candidates.size();
-//     combSum(0,target,candidates,comb,ans,n);
-//     return ans;
-// }
-pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-    let mut result: Vec<Vec<i32>> = Vec::new();
-    let mut stack: Vec<i32> = Vec::new();
 
-    fn dfs(array: &[i32], target: i32, stack: &mut Vec<i32>, result: &mut Vec<Vec<i32>>) {
-        if target == 0 {
-            result.push(stack.clone());
+pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
+    let mut nums = nums;
+    nums.retain(|n| n > &0);
+    let n = nums.len();
+    for i in 0..n {
+        while nums[i] <= n as i32 && nums[(nums[i] - 1) as usize] != nums[i] {
+            let j = (nums[i] - 1) as usize;
+            nums.swap(i, j);
         }
-        if target <= 0 || array.is_empty() {
-            return;
-        }
-        let n = array[0];
-        println!("{array:?}");
-        stack.push(n);
-        dfs(array, target - n, stack, result);
-        stack.pop();
-        dfs(&array[1..], target, stack, result);
     }
-
-    dfs(&candidates, target, &mut stack, &mut result);
-
-    result
+    for (i, elem) in nums.into_iter().enumerate().take(n) {
+        let next = (i + 1) as i32;
+        if elem != next {
+            return next;
+        }
+    }
+    (n + 1) as i32
 }
 
 #[test]
 fn test_fn() {
-    assert_eq!(
-        combination_sum(vec![2, 3, 6, 7], 7),
-        vec![vec![2, 2, 3], vec![7]]
-    );
-    // assert_eq!(
-    //     combination_sum(vec![2, 3, 5], 8),
-    //     vec![vec![2, 2, 2, 2], vec![2, 3, 3], vec![3, 5]]
-    // );
-    // assert_eq!(combination_sum(vec![2], 1), []);
+    assert_eq!(first_missing_positive(vec![1, 2, 0]), 3);
+    assert_eq!(first_missing_positive(vec![3, 4, -1, 1]), 2);
+    assert_eq!(first_missing_positive(vec![7, 8, 9, 10, 11]), 1);
 }
