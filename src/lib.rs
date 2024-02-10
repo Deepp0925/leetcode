5dd82bd1346100622725eb8e9ab37938af33967c
Deep Patel <deepp0925@gmail.com>
2024-02-09 21:29:33 -0500

    problem 42: trap water

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,29 @@
 mod solutions_1_30;
 
-pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
-    let mut nums = nums;
-    nums.retain(|n| n > &0);
-    let n = nums.len();
-    for i in 0..n {
-        while nums[i] <= n as i32 && nums[(nums[i] - 1) as usize] != nums[i] {
-            let j = (nums[i] - 1) as usize;
-            nums.swap(i, j);
+pub fn trap(height: Vec<i32>) -> i32 {
+    let mut l = 0;
+    let mut r = height.len() - 1;
+    let mut lmax = height[l];
+    let mut rmax = height[r];
+    let mut res = 0;
+    while l < r {
+        res += if lmax < rmax {
+            l += 1;
+            lmax = lmax.max(height[l]);
+            lmax - height[l]
+        } else {
+            r -= 1;
+            rmax = rmax.max(height[r]);
+            rmax - height[r]
         }
     }
-    for (i, elem) in nums.into_iter().enumerate().take(n) {
-        let next = (i + 1) as i32;
-        if elem != next {
-            return next;
-        }
-    }
-    (n + 1) as i32
+
+    res
 }
 
 #[test]
 fn test_fn() {
-    assert_eq!(first_missing_positive(vec![1, 2, 0]), 3);
-    assert_eq!(first_missing_positive(vec![3, 4, -1, 1]), 2);
-    assert_eq!(first_missing_positive(vec![7, 8, 9, 10, 11]), 1);
+    assert_eq!(trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
+    assert_eq!(trap(vec![4, 2, 0, 3, 2, 5]), 9);
+    // assert_eq!(trap(vec![7, 8, 9, 10, 11]), 1);
 }
