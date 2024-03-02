3cf30d8da3f746c30353f76a8530bcd8d20105b4
Deep Patel <deepp0925@gmail.com>
2024-03-01 21:11:53 -0500

    add binary

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,73 @@
 mod solutions_1_30;
 
-pub fn trap(height: Vec<i32>) -> i32 {
-    let mut l = 0;
-    let mut r = height.len() - 1;
-    let mut lmax = height[l];
-    let mut rmax = height[r];
-    let mut res = 0;
-    while l < r {
-        res += if lmax < rmax {
-            l += 1;
-            lmax = lmax.max(height[l]);
-            lmax - height[l]
+pub fn add_binary(a: String, b: String) -> String {
+    if a == "0" {
+        return b;
+    } else if b == "0" {
+        return a;
+    }
+
+    // ensures the a always greater than or equal to b
+    let (mut a, mut b) = if a.len() >= b.len() { (a, b) } else { (b, a) };
+    let mut new_string = String::new();
+
+    let mut carry = false;
+    while let Some(last_a) = a.pop() {
+        if let Some(last_b) = b.pop() {
+            match (last_a, last_b) {
+                ('0', '0') => {
+                    if carry {
+                        carry = false;
+                        new_string.insert(0, '1');
+                    } else {
+                        new_string.insert(0, '0');
+                    }
+                }
+                ('1', '1') => {
+                    if carry {
+                        new_string.insert(0, '1');
+                    } else {
+                        carry = true;
+                        new_string.insert(0, '0');
+                    }
+                }
+                ('1', '0') | ('0', '1') => {
+                    if carry {
+                        new_string.insert(0, '0');
+                    } else {
+                        new_string.insert(0, '1');
+                    }
+                }
+                _ => unreachable!(),
+            }
         } else {
-            r -= 1;
-            rmax = rmax.max(height[r]);
-            rmax - height[r]
+            match (carry, last_a) {
+                (true, '1') => {
+                    new_string.insert(0, '0');
+                }
+                (false, '1') | (true, '0') => {
+                    new_string.insert(0, '1');
+                    carry = false;
+                }
+                (false, '0') => {
+                    new_string.insert(0, '0');
+                }
+                _ => unreachable!("heere"),
+            }
         }
     }
 
-    res
+    if &new_string[0..1] == "0" || carry {
+        new_string.insert(0, '1');
+    }
+
+    new_string
 }
 
 #[test]
 fn test_fn() {
-    assert_eq!(trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
-    assert_eq!(trap(vec![4, 2, 0, 3, 2, 5]), 9);
-    // assert_eq!(trap(vec![7, 8, 9, 10, 11]), 1);
+    assert_eq!(
+        add_binary("100".to_string(), "110010".to_string()),
+        "110110".to_string()
+    );
 }
